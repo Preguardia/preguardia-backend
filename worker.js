13fca263ff0873cfdea98c04026ac59fde2584f0
Adrián Mouly <adrian@mouly.com.ar>
2016-03-25 13:23:35 -0300

    Added new push message info

--- worker.js
+++ worker.js
@@ -8,12 +8,20 @@ var sender = new gcm.Sender('AIzaSyC6-cOT27aMoxhl-YDkZGJpdnU1cwanawg');
 var queueRef = new Firebase('https://medicapp.firebaseio.com/queue');
 
 var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
+    var jsonData = JSON.parse(data);
+
     // Read and process task data
     console.log(data);
+
+    var type = jsonData.type;
+    var consultationId = jsonData.consultationId;
+    var topic = jsonData.topic;
 
     var message = new gcm.Message();
     message.addData('title', 'un titulo');
     message.addData('message', 'un mensaje');
+    message.addData('type', type);
+    message.addData('consultationId', 'consultationId');
 
     sender.sendNoRetry(message, { topic: '/topics/global' }, function (err, response) {
     	if(err) {
