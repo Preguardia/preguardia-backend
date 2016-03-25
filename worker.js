a71537efa920bdd7228682e1aad721d16ac64a56
Adrián Mouly <adrian@mouly.com.ar>
2016-03-25 00:49:28 -0300

    Added GCM integration

--- worker.js
+++ worker.js
@@ -1,17 +1,36 @@
 var Queue = require('firebase-queue');
 var Firebase = require('firebase');
+var gcm = require('node-gcm');
+
+// Set up the sender with you API key
+var sender = new gcm.Sender('AIzaSyBQu1KVXvMlmk0uqJOpAZXYfzKrwFpo8Go');
 
 var queueRef = new Firebase('https://medicapp.firebaseio.com/queue');
 
 var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
     // Read and process task data
     console.log(data);
+
     // Update the progress state of the task
-    setTimeout(function() {
-        progress(50);
-    }, 500);
+    // setTimeout(function() {
+    //     progress(50);
+    // }, 500);
+
+    var message = new gcm.Message();
+    message.addData('key1', 'msg1');
+
+    sender.sendNoRetry(message, { topic: '/topics/global' }, function (err, response) {
+    	if(err) {
+            console.error(err);
+        } else {
+            console.log(response);
+
+            resolve();
+        }
+    });
+
     // Finish the job asynchronously
-    setTimeout(function() {
-        resolve();
-    }, 1000);
+    // setTimeout(function() {
+    //     resolve();
+    // }, 1000);
 });
