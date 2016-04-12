a872a80fc53e9042e4f7041913cf5936fe3d150d
Adrián Mouly <adrian@mouly.com.ar>
2016-04-12 01:16:00 -0300

    Refactor Worker

--- worker.js
+++ worker.js
@@ -1,11 +1,15 @@
 var Queue = require('firebase-queue');
 var Firebase = require('firebase');
 var gcm = require('node-gcm');
- 
+
 const FIREBASE_URL = 'https://medicapp.firebaseio.com';
 const FIREBASE_USERS = 'users';
 const FIREBASE_QUEUE = 'queue';
 const FIREBASE_USER_REGISTER_ID = 'gcmRegisterId';
+
+const FIREBASE_TASK_CONSULTATION_NEW = 'new-consultation';
+const FIREBASE_TASK_CONSULTATION_APPROVED = 'consultation-approved';
+const FIREBASE_TASK_MESSAGE_NEW = 'message-new';
 
 const GCM_SENDER_ID = 'AIzaSyC6-cOT27aMoxhl-YDkZGJpdnU1cwanawg';
 const GCM_TOPIC_MEDIC = '/topics/medic';
@@ -16,7 +20,7 @@ var sender = new gcm.Sender(GCM_SENDER_ID);
 var queueRef = new Firebase(FIREBASE_URL + '/' + FIREBASE_QUEUE);
 var usersRef = new Firebase(FIREBASE_URL + '/' + FIREBASE_USERS);
 
-var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
+var queue = new Queue(queueRef, function (data, progress, resolve, reject) {
     var message = new gcm.Message();
 
     // Read and process task data
@@ -29,49 +33,67 @@ var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
     message.addData('type', type);
     message.addData('consultationId', consultationId);
 
-    if (type == 'new-consultation') {
-        message.addData('title', 'Nueva consulta médica');
-        message.addData('message', content);
+    // Handle each type of Task
+    switch (type) {
 
-        console.log("> New Consultation");
+        case FIREBASE_TASK_CONSULTATION_NEW:
 
-        sender.sendNoRetry(message, { topic: GCM_TOPIC_MEDIC }, function (err, response) {
-            if(err) {
-                console.error("< New Consultation - Notification not sent - Error: " + err);
-            } else {
-                console.log("< New Consultation - Notification sent - Response: " + response);
+            console.log("> New Consultation");
 
-                resolve();
-            }
-        });
-    } else if (type == 'consultation-approved') {
-        message.addData('title', 'Consulta aprobada');
-        message.addData('message', content);
+            message.addData('title', 'Nueva consulta médica');
+            message.addData('message', content);
 
-        console.log("> Consultation Approved");
-
-        var patientId = data.patientId;
-
-        // Retrieve GCM Token
-        usersRef.child(patientId).child(FIREBASE_USER_REGISTER_ID).once("value", function(data) {
-            var registerId = data.val();
-            var registrationTokens = [];
-
-            console.log("- User regId found: " + registerId);
-
-            // Add Register ID
-            registrationTokens.push(registerId);
-
-            // Send notification for that Patient
-            sender.sendNoRetry(message, { registrationTokens: registrationTokens }, function (err, response) {
-                if(err) {
-                    console.error("< Consultation Approved - Notification not sent - Error: " + err);
+            sender.sendNoRetry(message, {topic: GCM_TOPIC_MEDIC}, function (err, response) {
+                if (err) {
+                    console.error("< New Consultation - Notification not sent - Error: " + err);
                 } else {
-                    console.log("< Consultation Approved - Notification sent - Response: " + response);
+                    console.log("< New Consultation - Notification sent - Response: " + response);
 
                     resolve();
                 }
             });
-        });
+
+            break;
+
+        case FIREBASE_TASK_CONSULTATION_APPROVED:
+            var patientId = data.patientId;
+
+            console.log("> Consultation Approved");
+
+            message.addData('title', 'Consulta aprobada');
+            message.addData('message', content);
+
+            // Retrieve GCM Token
+            usersRef.child(patientId).child(FIREBASE_USER_REGISTER_ID).once("value", function (data) {
+                var registerId = data.val();
+                var registrationTokens = [];
+
+                console.log("- User regId found: " + registerId);
+
+                // Add Register ID
+                registrationTokens.push(registerId);
+
+                // Send notification for that Patient
+                sender.sendNoRetry(message, {registrationTokens: registrationTokens}, function (err, response) {
+                    if (err) {
+                        console.error("< Consultation Approved - Notification not sent - Error: " + err);
+                    } else {
+                        console.log("< Consultation Approved - Notification sent - Response: " + response);
+
+                        resolve();
+                    }
+                });
+            });
+
+            break;
+
+        case FIREBASE_TASK_MESSAGE_NEW:
+
+            console.log("> New Message");
+
+            break;
+
+        default:
+            console.log("> Default task type");
     }
 });
