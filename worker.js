37c0f59d0825323109173459353f8bf13724ffa3
Adrián Mouly <adrian@mouly.com.ar>
2016-04-18 16:18:06 -0300

    Refactor worker

--- worker.js
+++ worker.js
@@ -16,7 +16,10 @@ const FIREBASE_CONSULTATION_ID = 'consultationId';
 const GCM_SENDER_ID = 'AIzaSyC6-cOT27aMoxhl-YDkZGJpdnU1cwanawg';
 const GCM_TOPIC_MEDIC = '/topics/medic';
 
-// Set up the sender with you API key
+const GCM_NOTIFICATION_TYPE = 'type';
+const GCM_NOTIFICATION_TITLE = 'title';
+const GCM_NOTIFICATION_MESSAGE = 'message';
+
 var sender = new gcm.Sender(GCM_SENDER_ID);
 
 var queueRef = new Firebase(FIREBASE_URL + '/' + FIREBASE_QUEUE);
@@ -25,15 +28,10 @@ var usersRef = new Firebase(FIREBASE_URL + '/' + FIREBASE_USERS);
 var queue = new Queue(queueRef, function (data, progress, resolve, reject) {
     var message = new gcm.Message();
 
-    // Read and process task data
-    //console.log(data);
-
+    // Read task content
     var type = data.type;
     var content = data.content;
     var consultationId = data.consultationId;
-
-    message.addData('type', type);
-    message.addData(FIREBASE_CONSULTATION_ID, consultationId);
 
     // Handle each type of Task
     switch (type) {
@@ -42,8 +40,10 @@ var queue = new Queue(queueRef, function (data, progress, resolve, reject) {
 
             console.log("> New Consultation");
 
-            message.addData('title', 'Nueva consulta médica');
-            message.addData('message', content);
+            message.addData(GCM_NOTIFICATION_TYPE, type);
+            message.addData(GCM_NOTIFICATION_TITLE, 'Nueva consulta médica');
+            message.addData(GCM_NOTIFICATION_MESSAGE, content);
+            message.addData(FIREBASE_CONSULTATION_ID, consultationId);
 
             sender.sendNoRetry(message, {topic: GCM_TOPIC_MEDIC}, function (err, response) {
                 if (err) {
@@ -62,8 +62,10 @@ var queue = new Queue(queueRef, function (data, progress, resolve, reject) {
 
             console.log("> Consultation Approved");
 
-            message.addData('title', 'Consulta aprobada');
-            message.addData('message', content);
+            message.addData(GCM_NOTIFICATION_TYPE, type);
+            message.addData(GCM_NOTIFICATION_TITLE, 'Consulta aprobada');
+            message.addData(GCM_NOTIFICATION_MESSAGE, content);
+            message.addData(FIREBASE_CONSULTATION_ID, consultationId);
 
             // Retrieve GCM Token
             usersRef.child(patientId).child(FIREBASE_USER_REGISTER_ID).once("value", function (data) {
@@ -94,8 +96,10 @@ var queue = new Queue(queueRef, function (data, progress, resolve, reject) {
 
             console.log("> New Message");
 
-            message.addData('title', 'Nuevo mensaje');
-            message.addData('message', content);
+            message.addData(GCM_NOTIFICATION_TYPE, type);
+            message.addData(GCM_NOTIFICATION_TITLE, 'Nuevo mensaje');
+            message.addData(GCM_NOTIFICATION_MESSAGE, content);
+            message.addData(FIREBASE_CONSULTATION_ID, consultationId);
 
             // Retrieve GCM Token for User
             usersRef.child(userId).child(FIREBASE_USER_REGISTER_ID).once("value", function (data) {
